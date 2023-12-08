16b5cab36653c26821414153842b39cd0387e6f9
Rajesh Lamgade <w0851452@myscc.ca>
2023-12-08 10:03:40 -0500

    Initial commit

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,85 @@
+# app.py
+
+from flask import Flask, render_template
+import pandas as pd
+import sqlite3
+import requests 
+from bs4 import BeautifulSoup
+import requests 
+
+app = Flask(__name__, template_folder='templates')
+
+
+def scrape_website(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', {'id': 'individualResults'})
+        headers = [header.text.strip() for header in table.find_all('th')]
+        data = []
+        for row in table.find_all('tr')[1:]:
+            row_data = [cell.text.strip() for cell in row.find_all('td')]
+            data.append(row_data)
+        df = pd.DataFrame(data, columns=headers)
+        df.columns = df.columns.str.lower()
+        df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+        return df
+    else:
+        print(f"Failed to retrieve the page. Status Code: {response.status_code}")
+        return None
+
+
+def process_data(df):
+    df['place'] = pd.to_numeric(df['place'], errors='coerce').astype('Int64')
+    df['bib'] = pd.to_numeric(df['bib'], errors='coerce').astype('Int64')
+    # df['time_seconds'] = pd.to_datetime(df['time'], format='%M:%S', errors='coerce').dt.minute * 60 + pd.to_datetime(df['time'], format='%M:%S', errors='coerce').dt.second
+    # df['gun_time_seconds'] = pd.to_datetime(df['gun time'], format='%M:%S', errors='coerce').dt.minute * 60 + pd.to_datetime(df['gun time'], format='%M:%S', errors='coerce').dt.second
+    if all(df.isnull().sum() / df.shape[0] < 5):
+        df_cleaned = df.dropna()
+        print("Rows with missing values dropped.")
+    else:
+        df_cleaned = df.fillna(df.mean())
+        print("Missing values filled with mean.")
+    return df_cleaned
+
+def create_database(df, database_name='race_results_database.db', table_name='race_results_table'):
+    conn = sqlite3.connect(database_name)
+    df.to_sql(table_name, conn, index=False, if_exists='replace')
+    conn.commit()
+    conn.close()
+    print("Data successfully stored in SQLite database.")
+
+
+def query_database(query):
+    conn = sqlite3.connect('race_results_database.db')
+    result = pd.read_sql_query(query, conn)
+    conn.close()
+    return result
+
+
+    
+
+
+@app.route('/')
+def data():
+    print("h")
+    # Define the URL to scrape
+    url_to_scrape = "https://www.hubertiming.com/results/2017GPTR10K"
+
+    # Web scraping
+    df_raw = scrape_website(url_to_scrape)
+
+    # Data processing
+    df_processed = process_data(df_raw)
+
+    # Database creation
+    create_database(df_processed)
+
+    # Query database for display
+    query = "SELECT * FROM race_results_table"
+    df_display = query_database(query)
+
+    return render_template('home.html', data=df_display)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Race Results Web App
+
+Welcome to the Race Results Web App! This project is a web application that collects, processes, and displays race event data. It was developed as a final project for the Python course DAB111 - Data Analytics for Business in the first semester by group 13.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+
+## Installation
+
+To run this project, you need to have Python and pip installed on your system. Follow these steps:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/yourusername/race-results-web-app.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd race-results-web-app
+   ```
+
+3. Install the required packages using the following command:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## Usage
+
+1. After installation, run the Flask web application with the following command:
+
+   ```bash
+   python app.py
+   ```
+
+2. Open your web browser and go to [http://127.0.0.1:5000/](http://127.0.0.1:5000/) to access the landing page.
+
+3. Explore the project:
+   - Click on "View Data" to see the race results table.
+   - Click on "About" to learn more about the project.
+
+## Features
+
+- Landing Page: A welcome page introducing the project and group members.
+- Data Page: Displays a table of race results obtained through web scraping.
+- About Page: Provides information about the project, data collection, and acknowledgments.
+- Navigation Banner: Easily switch between the landing, data, and about pages.
+
+Feel free to explore and use the project for your own learning or adaptation. If you have suggestions or improvements, you can always create a fork of the repository and make your own modifications. Enjoy exploring the world of web scraping and Flask web development!
